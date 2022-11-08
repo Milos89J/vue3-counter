867e8ab8bb89f2c749f8fcc9e1a7d91e40308a07
Milos Jeremic <Milos.Jeremic@bdkadvokati.com>
2022-11-08 12:52:07 +0100

    final app

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,7 @@ import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import CountryView from "../views/CountryView.vue";
 import InformationView from "../views/InformationView.vue";
+import PageNotFound from "../views/404View.vue";
 
 
 const router = createRouter({
@@ -16,6 +17,10 @@ const router = createRouter({
             component: HomeView,
             name: "HomeView"
 
+        },
+        {
+            path: "/home",
+            redirect: "/"
         },
         {
             path: "/about",
@@ -33,6 +38,11 @@ const router = createRouter({
                     component: InformationView,
                 }
             ]
+        },
+        {
+            path: "/:catchall(.*)*",
+            component: PageNotFound,
+            pane: "PageNotFound"
         }
      ]
 });
--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -8,17 +8,25 @@ const router = useRouter();
 const countId = parseInt(route.params.id)
 
 const count = country.find((c) => c.id === countId);
+
+const showInformation = () => {
+  if (countId === 4) return;
+  return router.push(`/country/${countId}/information`)
+}
 </script>
 
 
 <template>
-  <div>
+  <div v-if="count">
     <h1>Country</h1>
     <p>{{ count.name }}</p>
     <p>{{ count.citizens }}</p>
     <p>{{ count.size }}</p>
-    <button @click="router.push(`/country/${countId}/information`)">Click for information</button>
+    <button @click="showInformation">Click for information</button>
     <RouterView />
+  </div>
+  <div v-else>
+    <h1>Information not found</h1>
   </div>
 </template>
 
